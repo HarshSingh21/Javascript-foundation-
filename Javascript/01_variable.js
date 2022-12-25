4849321d2272fbc00d304c12a557164bb1fe9abf
HarshSingh21 <singhharsh8283@gmail.com>
2022-12-26 00:57:49 +0530

    variable.js

---
+++ Javascript/01_variable.js
@@ -0,0 +1,16 @@
+const account_Id = 144552
+let accountEmail = "harsh@gmail.com"
+var accounPassword ="12123"
+accountCity ="Jaipur"
+
+
+accountCity ="Gorakhpur"
+
+//account_Id = 23   // not allowed to use in javascript 
+console.log(account_Id);
+
+/*  Prefer not to  use var because of issue in block and functional scope */
+console.table([accountEmail,accountCity,account_Id,accounPassword])
+
+
+// if you do not define variable javascript will print "undefined"  
